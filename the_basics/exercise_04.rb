46c0601de0ecc19a18b8f66c5c3593cebf65f95e
Tim Sullivan <timsullivan97@gmail.com>
2018-12-01 17:08:38 -0800

    add puts array exercise

---
+++ the_basics/exercise_04.rb
@@ -0,0 +1,6 @@
+# Use the dates from the previous example and store them in an array
+# Then make your program output the same thing as exercise 3.
+
+dates = [ 1981, 2001, 2013, 2004, 1975 ]
+
+puts dates
