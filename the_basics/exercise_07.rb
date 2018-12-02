f9f50c11e71fe7032371030d23563222f211b7a2
Tim Sullivan <timsullivan97@gmail.com>
2018-12-01 17:23:11 -0800

    add exercise 7

---
+++ the_basics/exercise_07.rb
@@ -0,0 +1,18 @@
+=begin
+Question:
+
+What does the following error message tell you?
+
+SyntaxErrorL (irb)2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>`
+  
+=end
+
+=begin
+Answer:
+
+The following error message tells me that in the interactive ruby development 
+environment there is a syntax error on line 2 where the user has written a 
+parentheses ) instead of a bracket }
+
+=end
