0ba5a2222f844b1f5152d2609829f99a754b15b1
Tim Sullivan <timsullivan97@gmail.com>
2018-12-01 17:43:47 -0800

    add variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program called name.rb that asks the user to type in their 
+name and then prints out a greeting message with their name included.
+=end
+
+puts "What is your name, playa?"
+name = gets.chomp
+
+puts "Nice, #{name}, playa."
