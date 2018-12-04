28d543a446a20ad96db87487e78498210f8dc2cb
Tim Sullivan <timsullivan97@gmail.com>
2018-12-03 19:57:29 -0800

    complete exercise 1

---
+++ methods/exercise_01.rb
@@ -0,0 +1,9 @@
+# Write a program that prints a greeting message. This program 
+# should contain a method called greeting that takes a name
+# parameter and returns a string
+
+def greeting(name)
+  puts "Greetings, #{name}. Saucy name breh."
+end
+
+puts greeting("Tim")
