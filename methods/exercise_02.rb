82191fed93d58e0229589ec868229862cca8152f
Tim Sullivan <timsullivan97@gmail.com>
2018-12-03 20:01:04 -0800

    add exercise 2

---
+++ methods/exercise_02.rb
@@ -0,0 +1,14 @@
+=begin
+  What do the following expressions evaluate to?
+
+  1. x = 2 - Evaluates to 2
+
+  2. puts x = 2 - Evaluates to 2
+
+  3. p name = "Joe" - Evaluates to the string "Joe"
+
+  4. four = "four" - Evaluates to the string "Four"
+
+  5. print something = "nothing" - prints out "nothing=> nil" Evaluates to nil?
+  
+=end
