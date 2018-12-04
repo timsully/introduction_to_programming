d650f50cd4239c44fbb1911b6f2971ef7e526d27
Tim Sullivan <timsullivan97@gmail.com>
2018-12-03 19:28:40 -0800

    add methods exercise

---
+++ methods/methods.rb
@@ -0,0 +1,28 @@
+# A method definition that does not mutate the caller but still 
+# returns the same value.
+
+# def some_method(number)
+#  number = 7 # this is implicitly returned by the method
+#end
+#
+#a = 5
+# when passing in 5, the number variable is reassigned the value 7
+# which DOES NOT affect a's value because number is scoped at the method 
+# definition level and a's value is unchanged
+#some_method(a)
+#puts a
+
+# When we perform some action on the argument that mutates
+# the caller, we can in fact permanently alter variables outside
+# the method definition's scope
+
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+
+
+p "Before mutate method: #{a}"
+mutate(a)
+p "After mutate method: #{a}"
