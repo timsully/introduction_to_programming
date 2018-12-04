4fe232f3952fb4b967c79914987428c348a56592
Tim Sullivan <timsullivan97@gmail.com>
2018-12-03 19:55:26 -0800

    update w/ comments and return

--- methods/return.rb
+++ methods/return.rb
@@ -1,9 +1,13 @@
-def add_three(number)
-  number + 3
-end
-
-returned_value = add_three(4)
-puts returned_value
-
 # Ruby methods ALWAYS return the evaluated result of the last line
 # of the expression UNLESS an explicit return comes before it.
+
+def add_three(number)
+  return number + 3
+  number + 4
+end
+
+# Since add_three method call returns a value, we can then keep
+# calling methods on the returned value
+returned_value = add_three(5).times { puts "this should print 8 times" }
+puts returned_value
+
