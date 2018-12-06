1320e662703afc39dc973933f0ec7025949eebb2
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:39:55 -0800

    complete exercise 4

---
+++ flow_control/exercise_04.rb
@@ -0,0 +1,29 @@
+=begin
+1. '4' == 4 ? puts("TRUE") : puts("FALSE")
+
+ANSWER: Evaluates to FALSE because one is a strong and the other is an integer
+
+2. x = 2
+   if ((x * 3) / 2) == (4 + 4 - x - 3)
+     puts "Did you get it right?"
+   else
+     puts "Did you?"
+   end
+
+ANSWER: Evaluates to true which puts "Did you get it right?"
+
+3. y = 9
+   x = 10
+   if (x + 1) <= (y)
+     puts "Alright."
+   elsif (x + 1) >= (y)
+     puts "Alright now!"
+   elsif (y + 1) == x
+     puts "ALRIGHT NOW!"
+   else
+     puts "Alrighty!"
+   end
+
+ANSWER: Evaluates to puts "Alright Now!"
+
+=end
