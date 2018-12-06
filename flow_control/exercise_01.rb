184d966475d1af85de182d388ab63f9791d57979
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:16:02 -0800

    add boolean exercise

---
+++ flow_control/exercise_01.rb
@@ -0,0 +1,11 @@
+=begin
+
+1. (32 * 4) >= 129 ---- FALSE
+2. false != !true ---- FALSE
+3. true == 4 ---- FALSE
+4. false == (847 == '847') ---- TRUE
+5. (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false ---- TRUE
+
+5. false || false || true || false
+
+=end
