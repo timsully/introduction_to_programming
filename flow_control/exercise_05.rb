02eb622f9f01da79f6be3114a6762b8f1da78da9
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:44:52 -0800

    exercise challenge: refactor program from if to when statement

---
+++ flow_control/exercise_05.rb
@@ -0,0 +1,15 @@
+=begin
+Rewrite your program from exercise 3 using a case statement. 
+Wrap this new case statement in a method and make sure it still works.
+=end
+
+puts "Gimme a number brah: "
+user_input = gets.chomp.to_i
+
+if user_input <= 50
+  puts "This number is between 0 and 50, which is litty."
+elsif user_input <= 100
+  puts "This number is between 51 and 100, or is 100, sickkkk."
+else
+  puts "This number is above 100!"
+end
