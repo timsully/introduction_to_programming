e5b04e9c1087084f2482e78e9246c96334f8f2da
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:19:23 -0800

    add exercise_02

---
+++ flow_control/exercise_02.rb
@@ -0,0 +1,6 @@
+=begin
+Write a method that takes a string as argument. The method should
+return a new, all-caps version of the string, only if the string is
+longer than 10 characters. Example: change "hello world" to "HELLO WORLD"
+=end
+
