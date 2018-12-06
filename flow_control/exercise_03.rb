40680cc2e5329e0aed08505dc01b5163702d3859
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:31:33 -0800

    complete exercise 3

--- flow_control/exercise_03.rb
+++ flow_control/exercise_03.rb
@@ -3,3 +3,14 @@ Write a program that takes a number from the user between 0 and 100
 and reports back whether the number is between 0 and 50, 51 and 100, 
 or above 100.
 =end
+
+puts "Gimme a number brah: "
+user_input = gets.chomp.to_i
+
+if user_input <= 50
+  puts "This number is between 0 and 50, which is litty."
+elsif user_input <= 100
+  puts "This number is between 51 and 100, or is 100, sickkkk."
+else
+  puts "This number is above 100!"
+end
