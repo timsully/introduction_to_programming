39f7dfdab7d138bd9393998268cde8f8bb0d7ff3
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:25:17 -0800

    complete exercise 2 w/ some humor, lulululul, lul.

--- flow_control/exercise_02.rb
+++ flow_control/exercise_02.rb
@@ -4,3 +4,11 @@ return a new, all-caps version of the string, only if the string is
 longer than 10 characters. Example: change "hello world" to "HELLO WORLD"
 =end
 
+puts "Gimme a string: "
+user_input = gets.chomp.to_s
+
+if user_input.length > 10
+  puts user_input.upcase
+else
+  puts "deez nuts"
+end
