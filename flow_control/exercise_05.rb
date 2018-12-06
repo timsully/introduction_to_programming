773104d8728c69c31cc11f552121a21f4bc09aa6
Tim Sullivan <timsullivan97@gmail.com>
2018-12-05 19:49:34 -0800

    refactor exercise from if to case statement

--- flow_control/exercise_05.rb
+++ flow_control/exercise_05.rb
@@ -6,10 +6,11 @@ Wrap this new case statement in a method and make sure it still works.
 puts "Gimme a number brah: "
 user_input = gets.chomp.to_i
 
-if user_input <= 50
-  puts "This number is between 0 and 50, which is litty."
-elsif user_input <= 100
-  puts "This number is between 51 and 100, or is 100, sickkkk."
-else
-  puts "This number is above 100!"
+case 
+  when user_input <= 50
+    puts "This number is between 0 and 50, which is litty."
+  when user_input <= 100
+    puts "This number is between 51 and 100, or is 100, sickkkk."
+  else
+    puts "This number is above 100!"
 end
