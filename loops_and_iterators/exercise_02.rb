8df978ea5081a955e13694fa28f9cf1c2e3eb63a
Tim Sullivan <timsullivan97@gmail.com>
2018-12-06 20:24:30 -0800

    add exercise

---
+++ loops_and_iterators/exercise_02.rb
@@ -0,0 +1,14 @@
+=begin
+Write a while loop that takes input from the user, 
+performs an action, and only stops when the user types
+"STOP". Each loop can get info from the user.
+=end
+
+while typing
+  # ask user question
+
+  # get input from user
+  
+  # stops when user types "STOP"
+
+end
