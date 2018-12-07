05874e9ca1be46691736fa6475e67092d839ce52
Tim Sullivan <timsullivan97@gmail.com>
2018-12-06 20:29:47 -0800

    complete exercise 2, too much sauce

--- loops_and_iterators/exercise_02.rb
+++ loops_and_iterators/exercise_02.rb
@@ -4,11 +4,15 @@ performs an action, and only stops when the user types
 "STOP". Each loop can get info from the user.
 =end
 
+typing = String
+
 while typing
   # ask user question
-
+  puts "Gimme some data, big data!"
   # get input from user
-  
+  big_data_lul = gets.chomp
   # stops when user types "STOP"
-
+  if big_data_lul == "STOP"
+    break
+  end
 end
