6617eb58212d1a7920242a39a3002dfd54787a34
Tim Sullivan <timsullivan97@gmail.com>
2018-12-07 20:13:06 -0800

    add #each_with_index exercise

---
+++ loops_and_iterators/exercise_03.rb
@@ -0,0 +1,15 @@
+=begin
+Use the each_with_index method to iterate through an array of 
+your creation that prints each index and value of the array.
+=end
+
+# What is #each_with_index ?
+# each_with_index is an index method that calls block with two arguments,
+# the item and its index, for each item in enumerator, given arguments
+# are passed through to each()
+
+array = [ 'key-one', 'value-one', 'key-two', 'value-two', 'key-three', 'value-three' ]
+
+array.each_with_index do |index, value|  
+  puts "#{index} \n #{value}"
+end
