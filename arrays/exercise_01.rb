35bf458e7210f11ff729a529a7fe21ee92a924f8
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 15:35:22 -0800

    add exercise to find a number in an array

--- arrays/exercise_01.rb
+++ arrays/exercise_01.rb
@@ -2,3 +2,12 @@
 Below we have given you an array and a number. Write a program 
 that checks to see if the number appears in the array.
 =end
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if number && arr[1] === 3
+  puts "You have found the number 3!"
+else
+  puts "Three is not an integer in the array."
+end
