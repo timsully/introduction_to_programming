6424a88f8e88aab7d52f7cae9ea0f655c5795669
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 15:39:35 -0800

    refactor code to 3 lines of code

--- arrays/exercise_01.rb
+++ arrays/exercise_01.rb
@@ -6,8 +6,12 @@ that checks to see if the number appears in the array.
 arr = [1, 3, 5, 7, 9, 11]
 number = 3
 
-if number && arr[1] === 3
-  puts "You have found the number 3!"
-else
-  puts "Three is not an integer in the array."
+#if number && arr[1] === 3
+#  puts "You have found the number 3!"
+#else
+#  puts "Three is not an integer in the array."
+#end
+
+if.arr.include?(number)
+  puts "#{number} is indeed in the array."
 end
