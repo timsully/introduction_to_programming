6ee0c7de809988bb7a5983bb672c0bb6a70a7cf2
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 15:46:45 -0800

    add array exercise 2

---
+++ arrays/exercise_02.rb
@@ -0,0 +1,11 @@
+=begin
+What will the following programs return? What is the value
+of arr after each?
+=end
+
+# Answer to Question 1
+# Error thrown or return nil for number one? As the range is bigger
+# than the array specified.
+
+# Answer to Question 2
+# 
