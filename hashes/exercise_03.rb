f4d901ba34b9cc7cda8651682de9fb55cf026c69
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 19:40:52 -0800

    add loops for each challenge in exercise

--- hashes/exercise_03.rb
+++ hashes/exercise_03.rb
@@ -2,5 +2,25 @@
 Using some of Ruby's built-in Hash methods, write a program 
 that loops through a hash and prints all of the keys. Then 
 write a program that does the same thing except printing the 
-values. Finally, write a program that prints both. 
+values. Finally, write a program that prints both.
 =end
+
+football_clubs = {
+  chelsea: 'England',
+  barcelona: 'Spain',
+  dortmund: 'Germany',
+  chivas: 'mexico'
+}
+
+# Displays all keys
+puts "Displays all keys:"
+football_clubs.select { |k, v| puts k }
+puts "\n"
+
+# Displays all of the keys values
+puts "Displays all of the keys values:"
+football_clubs.select { |k, v| puts v }
+puts "\n"
+
+puts "Displays both key and values:"
+football_clubs.select { |k, v| puts k, v }
