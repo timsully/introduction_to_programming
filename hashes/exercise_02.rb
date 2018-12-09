9e126580767ff62aa433fee0a11512ee6e76ddec
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 18:53:33 -0800

    add exercise 2

---
+++ hashes/exercise_02.rb
@@ -0,0 +1,9 @@
+=begin
+Look at Ruby's merge method. Notice that it has two versions. What is 
+the difference between merge and merge!? Write a program that uses both 
+and illustrate the differences.
+
+Merge method leaves the original hash unmodified, but with a bang operator 
+it will make a destructive change to the original hash.
+=end
+
