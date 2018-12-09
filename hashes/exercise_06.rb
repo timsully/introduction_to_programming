f2c6b60bb96495418a5818785a645e7c32d7a64f
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 20:13:02 -0800

    add exercise challenge detail as comment

---
+++ hashes/exercise_06.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program with the given array that prints out groups of words
+that are anagrams. Anagrams are words that have the same exact letters
+in them but in a different order. Your output should look something like this: 
+
+["demo", "dome", "mode"]
+["neon", "none"]
+(etc)
+=end
