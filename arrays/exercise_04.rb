0161bb30d3000f4a0c50f6d5fad67057a037e4c4
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 16:17:42 -0800

    complete exercise 4

---
+++ arrays/exercise_04.rb
@@ -0,0 +1,9 @@
+# What does each method return in the following example?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+arr.index(5) # Returns 3
+
+arr.index[5] # Returns undefined method for #<Enumerator
+
+arr[5] # Returns 8
