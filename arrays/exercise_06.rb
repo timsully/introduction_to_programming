327dd028a194f6c0688c90f3dbe2087c39e3c974
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 16:24:40 -0800

    add exercise 6

---
+++ arrays/exercise_06.rb
@@ -0,0 +1,18 @@
+=begin
+You run the following code...
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+... and get the following error message:
+
+TypeError: no implicit conversion of String into Integer
+  from (irb):2:in `[]='
+  from (irb):2
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+What is the problem and how can it be fixed?
+
+ANSWER - You need to specify an index value to target the element to change its 
+         string value, breh.
+=end
