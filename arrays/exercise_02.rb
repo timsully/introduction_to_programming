d663a791024beb34d95ac206e70afd43bf379314
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 16:10:32 -0800

    add answers to exercise

--- arrays/exercise_02.rb
+++ arrays/exercise_02.rb
@@ -4,8 +4,9 @@ of arr after each?
 =end
 
 # Answer to Question 1
-# Error thrown or return nil for number one? As the range is bigger
-# than the array specified.
+# Returns int value of 1
+#     arr =  [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
 
 # Answer to Question 2
-# 
+# Returns [1, 2, 3]
+#      arr = [["b"], ["a", [1, 2, 3]]]
