44407e11e51ef08fd827f422bae4a533d4918b14
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 18:58:25 -0800

    add completion of exercise to demonstrate hash merge methods

--- hashes/exercise_02.rb
+++ hashes/exercise_02.rb
@@ -7,3 +7,13 @@ Merge method leaves the original hash unmodified, but with a bang operator
 it will make a destructive change to the original hash.
 =end
 
+person = { key: 'sweg', keytwo: 'sauce', keythree: 'saucy' }
+
+new_hash = { newkey: 'yup', anotha: 'yessir', damn: 'islit' }
+
+# Merge hash w/out modifying the original hash
+person.merge(new_hash)
+# => {:key=>"sweg", :keytwo=>"sauce", :keythree=>"saucy", :newkey=>"yup", :anotha=>"yessir", :damn=>"islit"}
+
+# Destructive hash, (run commands in IRB to understand its effects)
+person.merge!(new_hash)
