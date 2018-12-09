1fc3a6272f0a1d412537ca693a20e716fcddaf4d
Tim Sullivan <timsullivan97@gmail.com>
2018-12-08 16:28:35 -0800

    add exercise problem

---
+++ arrays/exercise_07.rb
@@ -0,0 +1,10 @@
+=begin
+Write a program that iterates over an array and builds a new array 
+that is the result of incrementing each value in the original array 
+by a value of 2. You should have two arrays at the end of this program, 
+The original array and the new array you've created. Print both arrays 
+to the screen using the p method instead of puts.
+=end
+
+array = [2, 5, 6, 7, 10, 12, 101]
+
