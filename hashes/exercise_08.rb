13ac1f31e7fea4bbc90d0680824767be401426dd
Tim Sullivan <timsullivan97@gmail.com>
2018-12-09 19:23:55 -0800

    add exercise 8, explanation of no keys in Array class

---
+++ hashes/exercise_08.rb
@@ -0,0 +1,7 @@
+=begin
+If you see this error, what do you suspect is the most likely problem?
+
+NoMethodError: undefined method `keys' for Array
+
+There is no method called keys for Array objects. Keys method is a hash method.
+=end
