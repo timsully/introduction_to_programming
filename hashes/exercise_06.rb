a423fffcf6bd82557b76b6325d16b7c02aea241c
Tim Sullivan <timsullivan97@gmail.com>
2018-12-09 19:13:47 -0800

    add anagram word sort challenge

--- hashes/exercise_06.rb
+++ hashes/exercise_06.rb
@@ -7,3 +7,32 @@ in them but in a different order. Your output should look something like this:
 ["neon", "none"]
 (etc)
 =end
+
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+# initialize a result hash
+result = {}
+
+# iterate over the array, each word in this case
+words.each do |word|
+  # sort each word into alphabetical order
+  # creates the keys of the result hash
+  key = word.split('').sort.join
+  # if key exists, append current word into value (array)
+  if result.has_key?(key)
+    result[key].push(word)
+  # otherwise, create a new key with the sorted word
+  else
+    result[key] = [word]
+  end
+end
+
+# Goes through each array in the hash and prints them
+# to display the values sorted in different ways as indicated
+# in the challenge, lit.
+result.each_value do |v|
+    puts "-----"
+    p v
+end
