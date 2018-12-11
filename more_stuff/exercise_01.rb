d21ee86c1ecfa250919b4ba1ddf8df824f9e2000
Tim Sullivan <timsullivan97@gmail.com>
2018-12-10 21:12:19 -0800

    add exercise problem

---
+++ more_stuff/exercise_01.rb
@@ -0,0 +1,6 @@
+=begin
+Write a program that checks if the sequence of characters "lab" 
+exists in the following strings. If it does exist, print out the word.
+=end
+
+check_strings_playa = ["laboratory", "experiment", "Pans Labrynth", "elaborate", "polar bear"]
