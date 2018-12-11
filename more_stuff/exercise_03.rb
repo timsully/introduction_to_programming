1968614cf78ef7a74a93afa1be3a7cc512a8d8a8
Tim Sullivan <timsullivan97@gmail.com>
2018-12-10 21:09:11 -0800

    add exercise 3, exception handling question

---
+++ more_stuff/exercise_03.rb
@@ -0,0 +1,7 @@
+=begin
+What is exception handling and what problem does it solve?
+
+Exception handling is a specific process that deals with errors
+in a manageable and predictable way. Allowing the rest of your code 
+block to execute when one portion fails before the whole block is executed.
+=end
