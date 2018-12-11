ea822b12f4ae5e3f653d61e5bb47d3036a139c72
Tim Sullivan <timsullivan97@gmail.com>
2018-12-10 21:21:03 -0800

    add launch school solution

--- more_stuff/exercise_01.rb
+++ more_stuff/exercise_01.rb
@@ -12,3 +12,18 @@ check_strings_playa.each do |string|
     puts "This string doesn't contain the characters, lab"
   end
 end
+
+# Launch School Solution
+def check_in(word)
+  if /lab/ =~ word
+    puts word
+  else
+    puts "No match"
+  end
+end
+
+check_in("laboratory")
+check_in("experiment")
+check_in("Pans Labrynth")
+check_in("elaborate")
+check_in("polar bear")
