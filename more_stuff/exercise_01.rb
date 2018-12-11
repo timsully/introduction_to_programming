094a72bca4db257ec7eb1c9cebf02a634401ec15
Tim Sullivan <timsullivan97@gmail.com>
2018-12-10 21:18:52 -0800

    add my solution to checking array of strings

--- more_stuff/exercise_01.rb
+++ more_stuff/exercise_01.rb
@@ -4,3 +4,11 @@ exists in the following strings. If it does exist, print out the word.
 =end
 
 check_strings_playa = ["laboratory", "experiment", "Pans Labrynth", "elaborate", "polar bear"]
+
+check_strings_playa.each do |string|
+  if string.include?("lab")
+    puts string
+  else
+    puts "This string doesn't contain the characters, lab"
+  end
+end
