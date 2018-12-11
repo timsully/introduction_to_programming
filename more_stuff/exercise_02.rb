1e1b6eab3415b9f7a5c4ab492781c184d3abbb11
Tim Sullivan <timsullivan97@gmail.com>
2018-12-10 21:07:11 -0800

    add exercise 2

---
+++ more_stuff/exercise_02.rb
@@ -0,0 +1,12 @@
+=begin
+What will the following program print to the screen? What will it return?
+
+Nothing is printed as the block is not being called with the call method
+in the execute method. Instead, the method returns a Proc object.
+=end
+
+def execute(&block)
+  block
+end
+
+execute { puts "Hello from inside the execute method!" }
