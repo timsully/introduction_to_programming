bf620deb543ed7de7e6d5b7c961e493ca492b196
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 20:37:53 -0800

    complete exercise 10

---
+++ exercises/exercise_10.rb
@@ -0,0 +1,8 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+# Yes.
+
+# hash values as arrays
+hash = {names: ['bob', 'joe', 'susan']}
+
+# array of hashes
+arr = [{name: 'bob'}, {name: 'joe'}, {name: 'susan'}]
