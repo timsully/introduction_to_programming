693df3135fe295889619bcc8f72d6560bc27a3b6
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 20:20:32 -0800

    complete exercise 8

---
+++ exercises/exercise_08.rb
@@ -0,0 +1,14 @@
+# Create a Hash using both Ruby syntax styles.
+# new syntax
+hash_one = {
+  symbol: "sweg",
+  symbol_two: "swego",
+  symbol_lit: "litsky"
+}
+
+# old syntax
+hash_two = {
+  :symbol => "sweg",
+  :symbol_two => "swego",
+  :symbol_lit => "litsky"
+}
