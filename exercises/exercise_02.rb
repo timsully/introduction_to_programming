2d5326df99b21cecc21bb38b73391d30d181accc
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 19:42:49 -0800

    complete exercise 2 challenge

--- exercises/exercise_02.rb
+++ exercises/exercise_02.rb
@@ -2,8 +2,10 @@
 
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-array.each { |num| p num }
-
-#array.each do |num|
-#  p num
-#end
+array.each do |num|
+  if num > 5
+    puts "#{num}"
+  else
+    next
+  end
+end
