7205a95316be9778edc93640043a6d1d0913b59f
Tim Sullivan <timsullivan97@gmail.com>
2018-12-11 20:17:54 -0800

    complete exercise 7

---
+++ exercises/exercise_07.rb
@@ -0,0 +1,7 @@
+=begin
+What are the major differences between an Array and a Hash?
+
+An array is an ordered list of element that you access via index.
+
+A hash has key-value pairs, so you use keys to reference its values.
+=end
