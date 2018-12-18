78e8397713bb7bb298a36e6e6cf5e1ed13abd5d3
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 00:06:02 -0800

    add answer to exercise

--- exercises/exercise_11.rb
+++ exercises/exercise_11.rb
@@ -2,3 +2,14 @@
 Look at several Rails/Ruby online API sources and say 
 which one you like best and why.
 =end
+
+=begin 
+Voight-Kampff gem is a Ruby gem that detects bots, 
+spiders, crawlers and replicants. Easily detects 
+a request and if it's coming from a crawler, spider, etc.
+Helpful in analytics such as page hit tracking.
+
+I personally like this because it has to do with detecting
+automated bots and bots are what got me into programming
+in the first place thanks to design tools. BOOSH! SPAGAT!
+=end
