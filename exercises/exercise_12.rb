6d6cb654f118a483531e9e3694daa3f254b721a1
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 00:10:53 -0800

    add data structure exercise

---
+++ exercises/exercise_12.rb
@@ -0,0 +1,9 @@
+=begin
+Given the following data strtuctures. Write a program 
+that moves the information from the array into the empty 
+hash that applies to the correct person.
+=end
+
+contact_data = [["joe@email.com", "123 Main St.", "555-123-4567"], ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
