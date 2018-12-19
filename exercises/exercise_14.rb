b65e597de47b3caf9f24f6cfd5a4dd41d735e029
Tim Sullivan <timsullivan97@gmail.com>
2018-12-18 20:50:09 -0800

    add exercise 14 question

---
+++ exercises/exercise_14.rb
@@ -0,0 +1,17 @@
+=begin
+In exercise 12, we manually set the contacts hash values one by one.
+Now, programatically loop or iterate over the contacts hash from exercise
+12, and populate the associated data form the contact_data array. Hint:
+You will probably need to iterate over( [:email, :address, :phone] ), and
+some helpful methods might be the Array shift and first methods.
+
+Note that this exercise is ony concerned with dealing with 1 entry in the 
+contacts hash, like this:
+
+contact_data = ["joe@email.com", "123 Main St.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+
+As a bonus, see if you can figure out how to make it work with multiple
+entries in the contacts hash.
+=end
+
