791cc399d9f8beab36910cbd84c1d7dd6dde4ac4
Tim Sullivan <timsullivan97@gmail.com>
2019-01-03 20:52:14 -0800

    add exercise 16

---
+++ exercises/exercise_16.rb
@@ -0,0 +1,11 @@
+=begin
+Take the following array:
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+and turn it into a new array that consists of strings containing one word.
+Look into using Array's map and flatten methods, as well as String's split method.
+=end
+
+a = ['white snow', 'winter wonderland', 'melting ice', 'slippery sidewalk', 'salted roads', 'white trees']
