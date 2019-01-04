e0b25b98a938c725f42aa2c11b4e9f423d71b0c3
Tim Sullivan <timsullivan97@gmail.com>
2019-01-03 21:16:57 -0800

    complete exercise 16

--- exercises/exercise_16.rb
+++ exercises/exercise_16.rb
@@ -9,3 +9,12 @@ Look into using Array's map and flatten methods, as well as String's split metho
 =end
 
 a = ['white snow', 'winter wonderland', 'melting ice', 'slippery sidewalk', 'salted roads', 'white trees']
+
+# assign variable a equal to a.map method to split array of strings so that
+# each element(string) only contains one word
+a = a.map { |x| x.split(" ") }
+# re-assign a equal to a.flatten which will flatten the values returned the
+# method above
+a = a.flatten
+# display results
+p a
