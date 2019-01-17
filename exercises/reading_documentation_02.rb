de29849cac543533126f4f3af7bd4ffcb4b9baba
Tim Sullivan <timsullivan97@gmail.com>
2019-01-16 20:50:56 -0800

    add optional args exercise

--- exercises/reading_documentation_02.rb
+++ exercises/reading_documentation_02.rb
@@ -1,3 +1,5 @@
+# Required Arguments Exercise
+
 =begin
   Assume you have this Array:
   How would you use Array#insert to insert the numbers 5, 6, 7
@@ -9,3 +11,14 @@ a = %w(a b c d e)
 # At the third index of the array it adds the following numbers
 # 5, 6, and 7 to the array between letters 'c' and 'd'
 a.insert(3, 5, 6, 7)
+
+
+# Optional Arguments
+# Assume you have the following code:
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect
+# Pass in a comma which causes s to be split on commas
+puts s.split(',').inspect
+# Returns a 2 element array
+puts s.split(',', 2).inspect
