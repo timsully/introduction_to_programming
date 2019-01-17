d1c5a291edd50b5d2bd47f6946a5ab41ad903eb1
Tim Sullivan <timsullivan97@gmail.com>
2019-01-16 21:22:22 -0800

    add exercise

--- ruby_basics/variable_scope/whats_my_value_01.rb
+++ ruby_basics/variable_scope/whats_my_value_01.rb
@@ -1,4 +1,19 @@
 =begin
+Question:
 What will the following code print and why? 
 Don't run it until you have tried to answer.
+
+Answer (Wrong):
+The value 7 is assigned to the variable a which is
+passed into the function my_value which increments
+up by 10 making a = 17 when displaying the output
 =end
+
+a = 7
+
+def my_value(b)
+  b += 10
+end
+
+my_value(a)
+puts a
