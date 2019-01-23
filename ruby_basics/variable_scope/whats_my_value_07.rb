2ad7edf232bc42cd890a9387a10f70cd12d0bf7f
Tim Sullivan <timsullivan97@gmail.com>
2019-01-22 20:57:41 -0800

    add correct exercise answer

--- ruby_basics/variable_scope/whats_my_value_07.rb
+++ ruby_basics/variable_scope/whats_my_value_07.rb
@@ -3,7 +3,10 @@ What will the following code print, and why? Don't run the code until
 you have tried to answer.
 
 Answer:
-The following code prints 7.
+The following code prints 3. Block variable scoping rules allow you to 
+access local variable that were intialized in an outer scope. The most 
+recent assignment of our array will be to the last element of our array.
+Which is the integer 3.
 =end
 a = 7
 array = [1, 2, 3]
