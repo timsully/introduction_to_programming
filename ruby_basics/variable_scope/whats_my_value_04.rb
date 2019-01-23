bf0b46c3d7d1d0084e90b44e86e89f51f55283a8
Tim Sullivan <timsullivan97@gmail.com>
2019-01-22 20:25:38 -0800

    add correct answer

--- ruby_basics/variable_scope/whats_my_value_04.rb
+++ ruby_basics/variable_scope/whats_my_value_04.rb
@@ -3,7 +3,10 @@ What will the following code print, and why? Don't run the code until
 you have tried to answer.
 
 Answer:
-The following code prints the string "Xyzzy"
+Strings are mutable and can be modified. The method String#[]= is a mutating
+method; it actually modifies the string. Since we are actually modifying the 
+string referenced by b, and b references the same string as a, the result of
+printing a shows an update to the value of the string.
 =end
 a = "Xyzzy"
 
