f57393f02a9f1816a3fd6d986f2b1ba391e5d23b
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 20:21:02 -0800

    re-order outside loops break

--- ruby_basics/loops_01/loopception.rb
+++ ruby_basics/loops_01/loopception.rb
@@ -6,12 +6,12 @@ stops after the first iteration.
 
 loop do
   puts 'This is the outer loop.'
-  break
+
   loop do
     puts 'This is the inner loop.'
     break
   end
-  
+  break  
 end
 
 puts 'This is outside all loops.'
