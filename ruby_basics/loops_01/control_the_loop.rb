c88bf71f83cce6297663ed6844513df984379638
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 20:36:58 -0800

    add launch school correct answer to exercise

--- ruby_basics/loops_01/control_the_loop.rb
+++ ruby_basics/loops_01/control_the_loop.rb
@@ -5,6 +5,12 @@ instead of just once.
 iterations = 1
 
 loop do
-  puts "Number of iterations = #{iterations} \n" * 5
-  break
+  puts "Number of iterations = #{iterations}"
+  # Each time the loop loops it will increment the value
+  # assigned to the variable called iterations. Which initially 
+  # has a value of one.
+  iterations += 1
+  # Will break the loop once the value assigned to the variable
+  # iterations is 5. LIT.
+  break if iterations > 5
 end
