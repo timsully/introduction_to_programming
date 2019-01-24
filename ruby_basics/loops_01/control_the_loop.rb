48bd20697baaf172fc65b54df2bd98ed9a0ad4bf
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 20:39:10 -0800

    update comments to better understand code

--- ruby_basics/loops_01/control_the_loop.rb
+++ ruby_basics/loops_01/control_the_loop.rb
@@ -8,7 +8,7 @@ loop do
   puts "Number of iterations = #{iterations}"
   # Each time the loop loops it will increment the value
   # assigned to the variable called iterations. Which initially 
-  # has a value of one.
+  # has a value of one. Syntax can also be interpreted as re-assignment
   iterations += 1
   # Will break the loop once the value assigned to the variable
   # iterations is 5. LIT.
