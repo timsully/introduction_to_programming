17a3f25f5624d84249d54bab24f043334160aeff
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 21:25:49 -0800

    complete while loop exercise

--- ruby_basics/loops_01/print_while.rb
+++ ruby_basics/loops_01/print_while.rb
@@ -2,8 +2,15 @@
 Using a while loop, print 5 random numbers between 0 and 99. 
 The code below shows an example of how to begin solving this exercise. 
 =end
+# Empty array assigned to the variable numbers
 numbers = []
 
-while <condition>
-  # ...
+# While the number of elements is less than 5
+# return and add the random numbers to the numbers array
+while numbers.size < 5
+  # Using the shovel operator to append each new
+  # number to the end of the array.
+  numbers << rand(100)
 end
+
+puts numbers
