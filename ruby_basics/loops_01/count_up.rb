92fa67717185143f0f0c9f5bf50ab9ee64abd149
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 21:28:02 -0800

    add exercise problem

---
+++ ruby_basics/loops_01/count_up.rb
@@ -0,0 +1,10 @@
+=begin
+The following code outputs a countdown from 10 to 1.
+Modify the code so that it counts from 1 to 10 instead.
+=end
+count = 10
+
+until count == 0
+  puts count
+  count -= 1
+end
