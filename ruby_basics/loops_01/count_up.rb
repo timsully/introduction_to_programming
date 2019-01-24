0e6b802f1228efd3cf4797d849a9c83d353c00e0
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 21:28:58 -0800

    complete exercise, lul

--- ruby_basics/loops_01/count_up.rb
+++ ruby_basics/loops_01/count_up.rb
@@ -2,9 +2,9 @@
 The following code outputs a countdown from 10 to 1.
 Modify the code so that it counts from 1 to 10 instead.
 =end
-count = 10
+count = 1
 
-until count == 0
+until count > 10
   puts count
-  count -= 1
+  count += 1
 end
