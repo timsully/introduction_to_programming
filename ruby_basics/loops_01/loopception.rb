b661df5cac84f47a85232fe2d894af03b4dac36f
Tim Sullivan <timsullivan97@gmail.com>
2019-01-23 20:20:25 -0800

    complete exercise

--- ruby_basics/loops_01/loopception.rb
+++ ruby_basics/loops_01/loopception.rb
@@ -6,10 +6,12 @@ stops after the first iteration.
 
 loop do
   puts 'This is the outer loop.'
-
+  break
   loop do
     puts 'This is the inner loop.'
+    break
   end
+  
 end
 
 puts 'This is outside all loops.'
