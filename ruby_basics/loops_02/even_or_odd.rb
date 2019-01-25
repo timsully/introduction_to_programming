ed6e2b3f44e65c0d6d3743e0397acb4ff71474c6
Tim Sullivan <timsullivan97@gmail.com>
2019-01-24 20:43:12 -0800

    add loops_02 directory and exercise

---
+++ ruby_basics/loops_02/even_or_odd.rb
@@ -0,0 +1,16 @@
+=begin
+Write a loop that prints numbers 1-5 and whether the number is even or odd.
+Use the code below to get started. 
+
+Expected Output:
+1 is odd!
+2 is even!
+3 is odd!
+4 is even!
+5 is odd!
+=end
+count = 1
+
+loop do
+  count += 1
+end
