49bf5fdd49377a3fea05669bb7327bf6cadc22f7
Tim Sullivan <timsullivan97@gmail.com>
2019-01-24 21:32:38 -0800

    complete exercise

---
+++ ruby_basics/loops_02/insert_numbers.rb
@@ -0,0 +1,16 @@
+=begin
+Modify the code below so that the user's input gets added
+to the numbers array. Stop the loop when the array contains
+5 numbers. 
+=end
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  
+  numbers.push(input)
+  break if numbers.size == 5
+end
+
+puts numbers
