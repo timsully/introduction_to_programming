b9b897e58cb92708e7e0733602de158a1fb2910f
Tim Sullivan <timsullivan97@gmail.com>
2019-01-31 20:29:08 -0800

    add exercise problem

---
+++ ruby_basics/loops_02/stop_counting.rb
@@ -0,0 +1,7 @@
+=begin
+The method below counts from 0 to 4. Modify the block so that it prints the
+current number and stops iterating when the current number equals 2.
+=end
+5.times do |index|
+  # ...
+end
