2fe08c68a8134f2f6517e53ded37a74ce01142e7
Tim Sullivan <timsullivan97@gmail.com>
2019-01-31 20:37:31 -0800

    add answer to problem

--- ruby_basics/loops_02/stop_counting.rb
+++ ruby_basics/loops_02/stop_counting.rb
@@ -2,6 +2,11 @@
 The method below counts from 0 to 4. Modify the block so that it prints the
 current number and stops iterating when the current number equals 2.
 =end
+
 5.times do |index|
-  # ...
+  if index < 3
+    puts index
+  else
+    break
+  end
 end
