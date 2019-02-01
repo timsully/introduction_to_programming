f8ad1911ca687d49a8c0e9fc944d68af458035ae
Tim Sullivan <timsullivan97@gmail.com>
2019-01-31 21:26:11 -0800

    add n00b exercise to repo

---
+++ ruby_basics/user_input/repeat_after_me.rb
@@ -0,0 +1,10 @@
+=begin
+Write a program that asks the user to type something in, after which your program
+should simply display what was entered. 
+
+Example:
+$ ruby repeater.rb
+>> Type anything you want:
+This is what I typed.
+This is what I typed.
+=end
