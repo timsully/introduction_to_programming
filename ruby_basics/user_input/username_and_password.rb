892caacabf5c3188639ae464471fb6fdd30683a8
Tim Sullivan <timsullivan97@gmail.com>
2019-02-04 21:13:27 -0800

    add lit answer to exercise

--- ruby_basics/user_input/username_and_password.rb
+++ ruby_basics/user_input/username_and_password.rb
@@ -5,13 +5,16 @@ program should solicit both the user name and password, then validate both,
 and issue a generic error message if one or both are incorrect; the error message
 should not tell the user which item is incorrect.
 =end
-PASSWORD = "SecreT"
+PASSWORD = "nunya"
+USER_NAME = "lul"
 
 loop do
   # Require user name and validate both
+  puts ">> Please enter your user name:"
+  username_try = gets.chomp
   puts ">> Please enter your password:"
   password_try = gets.chomp
-  break if password_try == PASSWORD
+  break if username_try && password_try == PASSWORD && USER_NAME
   puts ">> Invalid password!"
 end
 
