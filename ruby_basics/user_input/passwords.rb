5f76e3492ed7b13da360976f217c1c6647a338ac
Tim Sullivan <timsullivan97@gmail.com>
2019-02-04 20:58:59 -0800

    upload lit exercise answer

---
+++ ruby_basics/user_input/passwords.rb
@@ -0,0 +1,26 @@
+=begin
+Write a program that displays a welcome message, but only after
+the user enters the correct password, where the password is a string
+that is defined as a constant in your program. Keep asking for the
+password until the user enters the correct password.
+=end
+def lit_func()
+  lit_password = "alitpasswordisalitpassword"
+
+  puts "Welcome!"
+  sleep(1)
+  puts "What is a lit password, breh?"
+  user_input = gets.chomp
+
+  if user_input == lit_password
+  puts "Welcome!"
+    else
+  puts "Invalid password!"
+  sleep(1)
+  puts "HAHAHA! \nTry again!"
+  lit_func()
+  end
+
+end
+
+lit_func()
