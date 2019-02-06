5afe9fce36d3fc251c7aa53e26d670706244461a
Tim Sullivan <timsullivan97@gmail.com>
2019-02-05 18:53:09 -0800

    complete exercise

--- ruby_basics/user_input/dividing_numbers.rb
+++ ruby_basics/user_input/dividing_numbers.rb
@@ -13,3 +13,31 @@ back to a string without loss of information, false otherwise. It's not a perfec
 solution in that some inputs that are otherwise valid (such as 003) will fail, but
 it is sufficient for this exercise.
 =end
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
+numerator = nil
+loop do
+  puts ">> Please enter the numerator:"
+  numerator = gets.chomp
+
+  break if valid_number?(numerator)
+  puts ">> Invalid input. Only integers are allowed."
+end
+
+denominator = nil
+loop do
+  puts ">> Please enter the denominator:"
+  denominator = gets.chomp
+
+  if denominator == '0'
+    puts ">> Invalid input. A denominator of 0 is not allowed."
+  else
+    break if valid_number?(denominator)
+    puts ">> Invalid input. Only integers are allowed."
+  end
+end
+
+result = numerator.to_i / denominator.to_i
+puts "#{numerator} / #{denominator} is result #{result}"
