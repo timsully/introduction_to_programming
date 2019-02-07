e6b59ef61f828a96a8dbc5400fa6124638d74430
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 20:09:32 -0800

    add completed methods exercise

---
+++ ruby_basics/methods/make_and_model.rb
@@ -0,0 +1,10 @@
+=begin
+Using the following code, write a method called car that takes two
+arguments and prints a string containing values of both arguments.
+=end
+def car(make, model)
+  puts "Car: #{make}"
+  puts "Model: #{model}"
+end
+
+car('Toyota', 'Corolla')
