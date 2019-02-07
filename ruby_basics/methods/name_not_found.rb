dcc218d59af0331c8990421f6c7db11f10771d96
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 20:31:45 -0800

    add exercise problem

---
+++ ruby_basics/methods/name_not_found.rb
@@ -0,0 +1,9 @@
+=begin
+Write a method that accepts one argument, but doesn't require it. The parameter
+should default to the string "Bob" if no argument is given. The method's return
+value should be the value of the argument.
+
+The code should output true twice.
+=end
+puts assign_name("Kevin") == "Kevin"
+puts assign_name == "Bob"
