3d2e8d9cceb591679cde52024f8e482274a51b14
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 21:07:52 -0800

    solve exercise problem

--- ruby_basics/methods/multiply_the_sum.rb
+++ ruby_basics/methods/multiply_the_sum.rb
@@ -1,6 +1,15 @@
 =begin
 Write the following methods so that each output is true.
 =end
-puts add(2, 2) == 4
-puts add(5, 4) == 9
+def add(x, y)
+  x + y
+end
+
+def multiply(one, two)
+  # pass in the add method twice as arguments
+  one * two
+end
+
+puts add(2, 2) == 4 # true
+puts add(5, 4) == 9 # true
 puts multiply(add(2, 2), add(5, 4)) == 36
