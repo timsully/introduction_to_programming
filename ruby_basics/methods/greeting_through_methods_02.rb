ee4d98ee8e54b6321a5eae4a93a5abe654e811e2
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 20:04:01 -0800

    add completed methods exercise

--- ruby_basics/methods/greeting_through_methods_02.rb
+++ ruby_basics/methods/greeting_through_methods_02.rb
@@ -1,3 +1,16 @@
 =begin
-
+Write a method named great that invokes the following methods:
 =end
+def hello
+  "Hello"
+end
+
+def world
+  "World"
+end
+
+def greet
+  puts "#{hello} #{world}"
+end
+
+greet()
