39574c89f9bb86fb48837a7a1920511f526dfba0
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 21:25:26 -0800

    add exercise problem

---
+++ ruby_basics/methods/random_sentence.rb
@@ -0,0 +1,26 @@
+=begin
+The variables below are both assigned to arrays. The first one, names, contains 
+a list of names. The second one, activities, contains a list of activites. Write
+the methods name and activity so that they each take the appropiate array and
+return a random value from it. Then write the method sentence that combines both
+values into a sentence and returns it from the method.
+
+Example output:
+George went walking today!
+=end
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+def name(arr)
+  # take names array
+end
+
+def activities(array)
+  # take names array
+end
+
+def sentence(one, two)
+  # Combines both values into a sentence and returns it from the method
+end
+
+puts sentence(name(names), activity(activities))
