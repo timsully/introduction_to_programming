cbb03d14bf0cdad819b350aba23f67dfa10e61d5
Tim Sullivan <timsullivan97@gmail.com>
2019-02-06 20:01:05 -0800

    move exercise files to proper directory

---
+++ ruby_basics/methods/greeting_through_methods_01.rb
@@ -0,0 +1,14 @@
+=begin
+Write two methods, one that returns the string "Hello" and one
+that returns the string "World". The print both strings using
+#puts, combinging them into one sentence.
+=end
+def hello
+  return "Hello"
+end
+
+def world
+  return " World"
+end
+
+puts hello + world
---
+++ ruby_basics/methods/print_me_02.rb
@@ -0,0 +1,9 @@
+=begin
+Write a method named print_me that returns "I'm printing the return value!"
+when using the following code.
+=end
+def print_me
+  "I'm printing the return value!"
+end
+
+puts print_me
