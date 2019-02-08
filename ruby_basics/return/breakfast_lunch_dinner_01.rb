60b4eafdd681ad99c95d833ab9448e295b4ba4c9
Tim Sullivan <timsullivan97@gmail.com>
2019-02-07 20:40:41 -0800

    add return exercise

---
+++ ruby_basics/return/breakfast_lunch_dinner_01.rb
@@ -0,0 +1,13 @@
+=begin
+What will the following code print? Why? Don't run until
+you've attempted to answer.
+
+Answer Before: Displays the string Breakfast.
+
+Answer After: 
+=end
+def meal
+  return 'Breakfast'
+end
+
+puts meal
