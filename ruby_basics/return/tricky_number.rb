64d62b5614ac2fc180b9e3155b815e814de638cc
Tim Sullivan <timsullivan97@gmail.com>
2019-02-07 21:31:59 -0800

    add exercise

--- ruby_basics/return/tricky_number.rb
+++ ruby_basics/return/tricky_number.rb
@@ -4,7 +4,12 @@ attempted to answer.
 
 Answer Before: Returns 2?
 
-Answer After: 
+Answer After: Printing the return value of tricky_number method by prepending puts
+              when we call(invoke) the method. As we look at the method, we see the
+              conditional statement where the if clause from the if-else stament will
+              be evaluated every time. The assignment to number is quite useless because
+              it's not used anywhere else. HOWEVER, variable  assignment still returns
+              the value it was assigned to.
 =end
 def tricky_number
   if true
