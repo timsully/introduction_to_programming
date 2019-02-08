39fbf1765f5228ffa5d760ea6e6fe90885ec749c
Tim Sullivan <timsullivan97@gmail.com>
2019-02-07 20:46:51 -0800

    complete 2nd return exercise

---
+++ ruby_basics/return/breakfast_lunch_dinner_02.rb
@@ -0,0 +1,15 @@
+=begin
+What will the following code print? Why? Don't run it until you've
+attempted to answer. 
+
+Answer Before: Prints the string Evening as it is the only line to be
+               evaluated in the method meal.
+
+Answer After: Same as above^
+
+=end
+def meal
+  'Evening'
+end
+
+puts meal
