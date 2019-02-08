37187fb5059b256b2f9a8c44eb487ee444658f0d
Tim Sullivan <timsullivan97@gmail.com>
2019-02-07 20:53:32 -0800

    complete exercise 4

---
+++ ruby_basics/return/breakfast_lunch_dinner_04.rb
@@ -0,0 +1,15 @@
+=begin
+What will the following code print? Why? Don't run it until you've
+attempted to answer. 
+
+Answer Before: Prints both Dinner and Breakfast on separate lines.
+
+Answer After: 
+
+=end
+def meal
+  puts 'Dinner'
+  return 'Breakfast'
+end
+
+puts meal
