ee0a95aaf41215adf6e0e53cafbc887ea0fd3d9b
Tim Sullivan <timsullivan97@gmail.com>
2019-02-07 21:27:48 -0800

    add my answer to exercise

---
+++ ruby_basics/return/tricky_number.rb
@@ -0,0 +1,17 @@
+=begin
+What will the following code print? Why? Don't run it until you've
+attempted to answer. 
+
+Answer Before: Returns 2?
+
+Answer After: 
+=end
+def tricky_number
+  if true
+    number = 1
+  else
+    2
+  end
+end
+
+puts tricky_number
