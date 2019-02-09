da248fbd0ba7700567f7152d62437dd2fdec2103
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 15:13:46 -0800

    add solution to string exercise

--- ruby_basics/ignoring_case.rb
+++ ruby_basics/ignoring_case.rb
@@ -2,6 +2,23 @@
 Using the following code, compare the value of name with the string "RoGeR"
 while ignoring the case of both strings. Print true if the values are the 
 same; print false if they aren't. Then, perform the same case-insensitive 
-comparison, except compare the value of name with the string 'DAVE' instead
-of 'RoGeR'
+comparison, except compare the value of name with the string "DAVE" instead
+of "RoGeR"
+
+Expected output:
+true
+false
 =end
+name = 'Roger'
+
+if name.casecmp('RoGeR')
+  puts true
+elsif
+  puts false
+end
+
+if name == 'DAVE'
+  puts true
+elsif
+  puts false
+end
