971efc15734b829013fcff35ecf901bd6ba99ce9
Tim Sullivan <timsullivan97@gmail.com>
2019-02-08 21:23:26 -0800

    refactor code to launch school answer

--- ruby_basics/conditionals/cool_numbers.rb
+++ ruby_basics/conditionals/cool_numbers.rb
@@ -8,7 +8,7 @@ Fix the code so that "Other numbers are cool too!" gets a chance to be executed.
 =end
 number = rand(10)
 
-if number % 2 == 0
+if number == 5
   puts "5 is a cool number!"
 else
   puts "Other numbers are cool too!"
