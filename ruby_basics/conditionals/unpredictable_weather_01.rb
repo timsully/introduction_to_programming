c8ec4f2be6b2391894f80952c439a21592641343
Tim Sullivan <timsullivan97@gmail.com>
2019-02-08 20:07:49 -0800

    add answer to exercise

--- ruby_basics/conditionals/unpredictable_weather_01.rb
+++ ruby_basics/conditionals/unpredictable_weather_01.rb
@@ -4,3 +4,9 @@ In the code below, sun is randomly assigned as 'visible' or 'hidden'.
 Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
 =end
 sun = ['visible', 'hidden'].sample
+
+if sun == 'visible'
+  puts "The sun is so bright!"
+else
+  puts sun
+end
