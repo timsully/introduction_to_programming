af0ecaa51f8c85d37f75e2889071ade6864646ea
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 14:21:41 -0800

    add string exercise

---
+++ ruby_basics/strings/create_a_string.rb
@@ -0,0 +1,11 @@
+=begin
+Create an empty string using the String class and assign it to a variable.
+=end
+# Empty strings can only be created in two ways.
+# Use of a string literal
+empty_string = ""
+
+# Invoking String's ::new class method
+empty_string = String.new
+
+# Note that both these empty strings return an empty string as an object.
