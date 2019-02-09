8eb571802deb14815e1d6521c3dcc7c3a08c4654
Tim Sullivan <timsullivan97@gmail.com>
2019-02-08 20:10:03 -0800

    add exercise file

---
+++ ruby_basics/conditionals/unpredictable_weather_02.rb
@@ -0,0 +1,6 @@
+=begin
+In the code below, sun is randomly assigned as 'visible' or 'hidden'.
+
+Write an unless statement that prints "The clouds are blocking the sun!" if sun equals 'visible'.
+=end
+sun = ['visible', 'hidden'].sample
