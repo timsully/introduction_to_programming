725f4c168f5820865d943b99dc9ff18baf4d2e8e
Tim Sullivan <timsullivan97@gmail.com>
2019-02-08 21:30:22 -0800

    add answer

--- ruby_basics/conditionals/stoplight_03.rb
+++ ruby_basics/conditionals/stoplight_03.rb
@@ -4,10 +4,7 @@ Reformat the following case statement so that it only takes takes up 5 lines.
 stoplight = ['green', 'yellow', 'red'].sample
 
 case stoplight
-when 'green'
-  puts "Go!"
-when 'yellow'
-  puts "Slow down!"
-when 'red'
-  puts "Stop!"
+when 'green'  then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+else               puts 'Stop!'
 end
