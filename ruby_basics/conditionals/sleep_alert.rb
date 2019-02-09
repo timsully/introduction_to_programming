35aa9d797a8ee5f77b7f983740d467a28275d984
Tim Sullivan <timsullivan97@gmail.com>
2019-02-08 21:16:14 -0800

    add conditional exercise

---
+++ ruby_basics/conditionals/sleep_alert.rb
@@ -0,0 +1,16 @@
+=begin
+In the code below, status is randomly assigned as 'awake' or 'tired'.
+
+Write an if statement that returns "Be productive!" if status equals 'awake' and
+returns "Go to sleep!" otherwise. Then, assign the return value of the if statement
+to a variable and print that variable.
+=end
+status = ['awake', 'tired'].sample
+
+alert = if status == 'awake'
+          "Be productive!"
+        else
+          "Go to sleep!"
+        end
+
+puts alert
