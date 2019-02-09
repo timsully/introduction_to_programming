3c20b6564a68e8e3caca77a194c24ae28951eaf4
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 15:42:24 -0800

    complete string exercise

--- ruby_basics/goodbye_not_hello.rb
+++ ruby_basics/goodbye_not_hello.rb
@@ -3,7 +3,7 @@ Given the following code, invoke a destructive method on greeting so that
 Goodbye! is printed instead of Hello!
 =end
 greeting = "Hello!"
-# .gsub! allows us to replace specific characters within the
+# .gsub!(destructive) allows us to replace specific characters within the
 # string by modifying the original object
 greeting.gsub!("Hello", "Goodbye")
 puts greeting
