27dac4e94c749d0876ddebf7be493a6bc7c81d5a
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 16:23:24 -0800

    add exercise problem

---
+++ ruby_basics/arrays/more_than_one.rb
@@ -0,0 +1,8 @@
+=begin
+In the code below, an array containing different types of pets
+is assigned to pets.
+
+Select fish and lizard from pets at the same time, assign the return value
+to a variable named my_pets, then print the of my_pets.
+=end
+pets = ['cat', 'dog', 'fish', 'lizard']
