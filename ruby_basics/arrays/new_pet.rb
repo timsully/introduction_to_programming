7ae0059e264a6f08770b355121e30b2b3f98b4c3
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 16:18:25 -0800

    add array exercise

---
+++ ruby_basics/arrays/new_pet.rb
@@ -0,0 +1,10 @@
+=begin
+In the code below, an array containing different types of pets is assigned to pets.
+
+Select 'fish' from pets, assign the retun value to a variable named my_pet, 
+then print the value of my_pet.
+
+Expected output:
+I have a pet fish!
+=end
+pets = ['cat', 'dog', 'fish', 'lizard']
