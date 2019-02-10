8143a619f75c2dc906e73c7e750d259d4f8b7387
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 16:28:35 -0800

    complete array exercise

--- ruby_basics/arrays/more_than_one.rb
+++ ruby_basics/arrays/more_than_one.rb
@@ -6,3 +6,7 @@ Select fish and lizard from pets at the same time, assign the return value
 to a variable named my_pets, then print the of my_pets.
 =end
 pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pet = pets[2..3]
+
+puts "I have a pet #{my_pet[0]} and a pet #{my_pet[1]}!"
