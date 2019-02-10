ca678994f4505050a0db3ec9b8fedc8b53dc3178
Tim Sullivan <timsullivan97@gmail.com>
2019-02-09 16:09:33 -0800

    complete conditional statement exercise

---
+++ ruby_basics/are_you_there.rb
@@ -0,0 +1,18 @@
+=begin
+Using the following code, print true if colors includes the color
+'yellow' and print false if it doesn't. Then, print true if colors
+includes the color 'purple' and print false if it doesn't.
+=end
+colors = "blue pink yellow orange"
+
+if colors.include? 'yellow'
+  puts true
+else
+  puts false
+end
+
+if colors.include? 'purple'
+  puts true
+else
+  puts false
+end
