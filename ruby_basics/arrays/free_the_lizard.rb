f747920121499a1bfb172a7d1de84028eaeb343b
Tim Sullivan <timsullivan97@gmail.com>
2019-02-10 18:38:19 -0800

    add exercise problem

---
+++ ruby_basics/arrays/free_the_lizard.rb
@@ -0,0 +1,11 @@
+=begin
+In the code below, an array containing different types of pets is assigned to pets.
+Also, the return value of pets 
+
+Remove 'lizard' from my_pets then print the value of my_pets.
+
+Expected output:
+I have a pet fish!
+=end
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
