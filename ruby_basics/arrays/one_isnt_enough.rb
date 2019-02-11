1968f773c326ff85a2c2d550ebfe359dc37139e5
Tim Sullivan <timsullivan97@gmail.com>
2019-02-10 18:44:30 -0800

    add array exercise problem

---
+++ ruby_basics/arrays/one_isnt_enough.rb
@@ -0,0 +1,10 @@
+=begin
+Using the code below, select 'dog' from pets, add the return value to my_pets,
+then print the value of my_pets.
+
+Expected output:
+I have a pet fish and a pet dog!
+=end
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
