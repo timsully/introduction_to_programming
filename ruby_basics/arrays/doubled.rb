710c18760ad1f114b6171f84c8c58a62aea96e15
Tim Sullivan <timsullivan97@gmail.com>
2019-02-10 19:36:05 -0800

    add array map exercise problem

---
+++ ruby_basics/arrays/doubled.rb
@@ -0,0 +1,9 @@
+=begin
+In the code below, an array containing the numbers 1 through 5
+is assigned to numbers.
+
+Use Array#map to iterate over numbers and return a new array with each number
+doubled. Assign the returned array to a variable named doubled_numbers and
+print its value using #p.
+=end
+numbers = [1, 2, 3, 4, 5]
