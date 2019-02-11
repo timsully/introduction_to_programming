a61da539835c6ab612ca4050572725eb9303ca97
Tim Sullivan <timsullivan97@gmail.com>
2019-02-10 19:45:18 -0800

    add exercise problem

---
+++ ruby_basics/arrays/divisible_by_three.rb
@@ -0,0 +1,11 @@
+=begin
+In the code below, an array containing five numbers is assigned to numbers.
+
+Use Array#select to iterate over numbers and return a new array that contains
+only numbers divisible by three. Assign the returned array to a variable
+divisible by three and print its value using #p.
+
+Expected output:
+[9, 21, 39]
+=end
+numbers = [5, 9, 21, 26, 39]
