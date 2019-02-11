468e0aea1b57aa303ea86321bf692a571e6d1ce1
Tim Sullivan <timsullivan97@gmail.com>
2019-02-10 19:33:00 -0800

    complete exercise

---
+++ ruby_basics/arrays/what_color_are_you.rb
@@ -0,0 +1,15 @@
+=begin
+In the code below, an array containing different types of colors is
+assigned to colors.
+
+Use Array#each to iterate over colors and print each element.
+
+Expected output:
+I'm the color red!
+I'm the color yellow!
+I'm the color purple!
+I'm the color green!
+=end
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each { |x| puts "I'm the color #{x}!" }
