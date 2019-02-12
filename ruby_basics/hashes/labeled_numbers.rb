01525783954183a2725cc4677fbe18d6370b0c45
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 20:25:01 -0800

    add exercise problem: print hashing w/ string interpolation

---
+++ ruby_basics/hashes/labeled_numbers.rb
@@ -0,0 +1,13 @@
+=begin
+Use Hash#each to iterate over numbers and print each element's key/value pair.
+
+Expected output:
+A high number is 100.
+A medium number is 50.
+A low number is 10.
+=end
+number = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
