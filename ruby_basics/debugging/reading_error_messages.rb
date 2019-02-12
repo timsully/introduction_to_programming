ba80b4372bad6753b3de58f75148e163cfe067f9
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 21:20:19 -0800

    add debugging exercise

---
+++ ruby_basics/debugging/reading_error_messages.rb
@@ -0,0 +1,14 @@
+=begin
+You come across the following code. What errors does it raise for the 
+given examples and what exactly do the error messages mean?
+=end
+def find_first_nonzero_among(numbers)
+  numbers.each do |n|
+    return n if n.nonzero?
+  end
+end
+
+# Examples
+
+find_first_nonzero_among(0, 0, 1, 0, 2, 0)
+find_first_nonzero_among(1)
