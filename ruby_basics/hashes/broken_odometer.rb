6537861ee287ce9e2758cdb33be5fbec729a2325
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 20:18:37 -0800

    add exercise problem

---
+++ ruby_basics/hashes/broken_odometer.rb
@@ -0,0 +1,10 @@
+=begin
+Using the following code, delete the key :mileage and its associated
+value from car.
+=end
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000,
+  year:    2003
+}
