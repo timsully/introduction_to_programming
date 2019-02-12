a1c66eaebab83cf3863443f6787cc1b0c1049f99
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 20:42:03 -0800

    add exercise problem

---
+++ ruby_basics/hashes/divided_by_two.rb
@@ -0,0 +1,13 @@
+=begin
+Use Enumerable#map to iterate over numbers and return an array 
+containing each number divided by 2. Assign the returned array to a variable
+named half_numbers and print its value using #p.
+
+Expected output:
+[50, 25, 5]
+=end
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
