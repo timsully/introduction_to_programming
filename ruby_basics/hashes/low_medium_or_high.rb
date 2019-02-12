b1f50a0d6d8bd75a44a48dc6a24a2e61807ebebf
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 20:51:51 -0800

    add exercise problem

---
+++ ruby_basics/hashes/low_medium_or_high.rb
@@ -0,0 +1,12 @@
+=begin
+Use Hash#select to iterate over numbers and return a hash containing only 
+key-value pairs where the values is less than 25.
+
+Expected output:
+{:low=>10}
+=end
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
