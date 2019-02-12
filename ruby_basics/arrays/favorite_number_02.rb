8f2438cbf1816d0ab0e62845833330eaaa606eb2
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 19:48:32 -0800

    add exercise problem

---
+++ ruby_basics/arrays/favorite_number_02.rb
@@ -0,0 +1,12 @@
+=begin
+In the code below, a nested array containing three groups of names and
+numbers is assigned to favorites.
+
+Use Array#flatten to flatten favorites so that it's no longer a nested array.
+Then assign the flattened array to a variable named flat_favorites and print 
+its value using #p.
+
+Expected output:
+["Dave", 7, "Miranda", 3, "Jason", 11]
+=end
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]
