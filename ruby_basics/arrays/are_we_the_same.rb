56c634fa206bf9eea3671acac7e33326b7fbf606
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 19:58:55 -0800

    add conditional statement to compare arrays

--- ruby_basics/arrays/are_we_the_same.rb
+++ ruby_basics/arrays/are_we_the_same.rb
@@ -6,3 +6,13 @@ Compare array1 and array2 and print true or false based on whether they match.
 =end
 array1 = [1, 5, 9]
 array2 = [1, 9, 5]
+
+# Launch School Solution
+puts array1 == array2
+
+# My Solution
+if array1 == array2
+  puts true
+elsif
+  puts false
+end
