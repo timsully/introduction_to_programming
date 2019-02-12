abef3b366a879af9282c0bc24d1c5141a89273e5
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 21:03:10 -0800

    add my solution

--- ruby_basics/hashes/low_medium_or_high.rb
+++ ruby_basics/hashes/low_medium_or_high.rb
@@ -1,6 +1,7 @@
 =begin
 Use Hash#select to iterate over numbers and return a hash containing only 
-key-value pairs where the values is less than 25.
+key-value pairs where the values is less than 25. Assign the returned hash
+to a variable named low_numbers and print its value using #p.
 
 Expected output:
 {:low=>10}
@@ -10,3 +11,12 @@ numbers = {
   medium: 50,
   low:    10
 }
+
+low_numbers = numbers.select do |key, value|
+                if value < 25
+                  p key, value
+                end
+              end
+
+
+puts low_numbers
