174f705b0ff025545d76f7d71a80efda92657b36
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 19:54:00 -0800

    add solution: flatten array

--- ruby_basics/arrays/favorite_number_02.rb
+++ ruby_basics/arrays/favorite_number_02.rb
@@ -2,11 +2,16 @@
 In the code below, a nested array containing three groups of names and
 numbers is assigned to favorites.
 
-Use Array#flatten to flatten favorites so that it's no longer a nested array.
+Use Array#flatten to flatten favorites so that it"s no longer a nested array.
 Then assign the flattened array to a variable named flat_favorites and print 
 its value using #p.
 
 Expected output:
 ["Dave", 7, "Miranda", 3, "Jason", 11]
 =end
-favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]
+favorites = [["Dave", 7], ["Miranda", 3], ["Jason", 11]
+
+flat_favorites = favorites.flatten
+
+p flat_favorites
+p favorites
