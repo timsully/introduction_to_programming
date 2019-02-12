7e3498f1c7c446270e11a5d1df4f29ce586b441d
Tim Sullivan <timsullivan97@gmail.com>
2019-02-11 19:55:46 -0800

    add exercise problem

---
+++ ruby_basics/arrays/are_we_the_same.rb
@@ -0,0 +1,8 @@
+=begin
+In the code below, two arrays containing several numbers are assigned to two
+variables, array1, and array2.
+
+Compare array1 and array2 and print true or false based on whether they match.
+=end
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
