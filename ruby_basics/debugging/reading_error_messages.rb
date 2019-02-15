68f0bbb3dfe58b19fc2e3e0b0b6cbfe07f451279
Tim Sullivan <timsullivan97@gmail.com>
2019-02-14 20:14:46 -0800

    complete exercise w/ my solution

--- ruby_basics/debugging/reading_error_messages.rb
+++ ruby_basics/debugging/reading_error_messages.rb
@@ -1,6 +1,19 @@
 =begin
 You come across the following code. What errors does it raise for the 
 given examples and what exactly do the error messages mean?
+
+My Answer:
+First error when executing the file throws this ArgumentError message:
+
+"wrong number of arguments (given 6, expected 1) (ArgumentError)""
+Too many arguments are passed into the first function we call. 6 to
+be exact and the function only expects 1 to be passed in, therefore throwing
+the error message.
+
+When I comment out the first call of the method and execute the file again a 
+NoMethodError is thrown. Since integers do not have an each method, this raises
+a NoMethodError.
+
 =end
 def find_first_nonzero_among(numbers)
   numbers.each do |n|
@@ -10,5 +23,5 @@ end
 
 # Examples
 
-find_first_nonzero_among(0, 0, 1, 0, 2, 0)
+# find_first_nonzero_among(0, 0, 1, 0, 2, 0)
 find_first_nonzero_among(1)
