1f9886c899d1738a40b468ed0249192226bb68ae
Tim Sullivan <timsullivan97@gmail.com>
2019-02-14 20:35:00 -0800

    complete exercise

--- ruby_basics/debugging/multiply_by_five.rb
+++ ruby_basics/debugging/multiply_by_five.rb
@@ -1,12 +1,17 @@
 =begin
 When the user inputs 10, we expect the program to print 'The result is 50!',
 but that's not the output we see. Why not?
+
+My answer:
+Convert user input from string to integer.
 =end
 def multiply_by_five(n)
   n * 5
 end
 
 puts "Hello! Which number would you like to multiply by 5?"
-number = gets.chomp
+number = gets.chomp.to_i
+
+# multiply_by_five(number)
 
 puts "The result is #{multiply_by_five(number)}!"
