6ffb6774f21a5b2eed240a80affc5b1366b712cf
Tim Sullivan <timsullivan97@gmail.com>
2019-02-14 20:26:41 -0800

    add boolean values instead of strings

--- ruby_basics/debugging/weather_forecast.rb
+++ ruby_basics/debugging/weather_forecast.rb
@@ -4,11 +4,15 @@ sunny or cloudy day lies ahead. However, the output is the same every
 time the method is invoked. Why? Fix the code so that it behaves as expected.
 =end
 def predict_weather
-  sunshine = ['true', 'false'].sample
-
+  sunshine = [true, false].sample
+  
+  # Missing value comparison to evaluate random value generated from
+  # array sunshine to then print out the proper statement
   if sunshine
     puts "Today's weather will be sunny!"
-  else
+  elsif
     puts "Today's weather will be cloudy!"
   end
 end
+
+puts predict_weather()
