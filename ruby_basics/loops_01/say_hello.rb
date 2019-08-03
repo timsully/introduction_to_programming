463632b15fa3d57c2f0b75c30961fab3ee067e6c
Tim Sullivan <timsullivan97@gmail.com>
2019-08-03 16:40:50 -0700

    add another solution to exercise

--- ruby_basics/loops_01/say_hello.rb
+++ ruby_basics/loops_01/say_hello.rb
@@ -9,3 +9,13 @@ while say_hello
   say_hello += 1
   break if say_hello > 5
 end
+
+# or
+say_hello = true
+count = 0
+
+while say_hello
+  puts "Hello!"
+  count += 1
+  say_hello = false if count == 5
+end
